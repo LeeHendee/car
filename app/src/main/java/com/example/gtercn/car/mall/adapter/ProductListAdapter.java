188979ba8749387b5c25e797047d5be9446404c3
Lee <gu_shi120@163.com>
2017-12-27 21:56:43 +0800

    2017-12-27 开发产品列表，代码上传

---
+++ app/src/main/java/com/example/gtercn/car/mall/adapter/ProductListAdapter.java
@@ -0,0 +1,75 @@
+package com.example.gtercn.car.mall.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.gtercn.car.R;
+import com.example.gtercn.car.mall.entity.ProductListEntity;
+
+import org.w3c.dom.ls.LSException;
+
+import java.util.List;
+
+/**
+ * Created by Yan on 2017/12/27.
+ */
+
+public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.ProductionViewHolder> {
+
+    private static final String TAG = "ProductListAdapter";
+
+    private Context context;
+
+    private List<ProductListEntity.ResultBean> list;
+
+    public ProductListAdapter(Context context, List<ProductListEntity.ResultBean> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public ProductionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_production, parent, false);
+        ProductionViewHolder holder = new ProductionViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ProductionViewHolder holder, int position) {
+        ProductListEntity.ResultBean bean = list.get(position);
+        holder.titleTv.setText(bean.getGoods_title());
+        holder.priceTv.setText(bean.getPromotion_price() + "");
+        holder.soldTv.setText(bean.getSold_number() + "");
+        holder.reviewsTv.setText(bean.getGoods_synopsis());
+        holder.reviewsRateTv.setText(bean.getGoods_title());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list != null ? list.size() : 0;
+    }
+
+    class ProductionViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTv;
+        TextView priceTv;
+        TextView soldTv;
+        TextView reviewsTv;
+        TextView reviewsRateTv;
+        ImageView productIv;
+
+        public ProductionViewHolder(View itemView) {
+            super(itemView);
+            titleTv = (TextView) itemView.findViewById(R.id.tv_product_title);
+            priceTv = (TextView) itemView.findViewById(R.id.tv_price);
+            soldTv = (TextView) itemView.findViewById(R.id.tv_sold);
+            reviewsTv = (TextView) itemView.findViewById(R.id.tv_reviews);
+            reviewsRateTv = (TextView) itemView.findViewById(R.id.tv_good_reviews_rate);
+            productIv = (ImageView) itemView.findViewById(R.id.iv_product);
+        }
+    }
+}
--- app/src/main/java/com/example/gtercn/car/mall/view/ProductListActivity.java
+++ app/src/main/java/com/example/gtercn/car/mall/view/ProductListActivity.java
@@ -2,11 +2,15 @@ package com.example.gtercn.car.mall.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.gtercn.car.R;
 import com.example.gtercn.car.base.BaseActivity;
+import com.example.gtercn.car.mall.adapter.ProductListAdapter;
 
 /**
  * Created by Yan on 2017/12/24.
@@ -29,6 +33,12 @@ public class ProductListActivity extends BaseActivity {
 
     private TextView mSortTv;
 
+    private SwipeRefreshLayout mRefresh;
+
+    private RecyclerView mRecyclerView;
+
+    private ProductListAdapter mAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +47,18 @@ public class ProductListActivity extends BaseActivity {
 
     private void initView() {
         setContentView(R.layout.activity_production_list);
+        mComprehensiveSortTv = (TextView) findViewById(R.id.tv_comprehensive_sort);
+        mSaleSortTv = (TextView) findViewById(R.id.tv_sale_sort);
+        mPriceSortTv = (TextView) findViewById(R.id.tv_price_sort);
+        mSortTv = (TextView) findViewById(R.id.tv_sort);
+        mRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recy_production);
+        mBackIv = (ImageView) findViewById(R.id.iv_back);
+        mSearchTv = (TextView) findViewById(R.id.tv_search);
+
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+
     }
 
     @Override
